ff214e98877fe732749d375a86ae8fb01651bc29
jeske <jeske>
2001-05-15 07:38:44 +0000

    clean out the windows resources and fix icon

--- hz/os_win/resource.h
+++ hz/os_win/resource.h
@@ -1,16 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
-// Used by Donuts.rc
+// Used by donuts.rc
 //
 #define APP_ICON                        103
 #define ICON106                         106
 #define ICON107                         107
+#define IDB_BITMAP1                     114
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_RESOURCE_VALUE        115
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
